e43e84f8c7a75083314556c3ba54b228e6fd06da
komalpawar9973 <pawarkomal9973@gmail.com>
2026-07-28 13:49:14 +0530

    Add files via upload

---
+++ lab5.py
@@ -0,0 +1,29 @@
+n =int (input ("enter a number"))
+if n<5:
+    print(n,"is less than 5")
+    
+
+print("-- second code--")
+    
+n = int (input("enter a number"))
+if n<5:
+         print(n, "is less than 5")
+else:
+     print(n,"is not less than 5")   
+     
+print("--third code--")    
+  
+n = int(input("enter a number "))
+if n<5:
+    print(n, "is less than 5")
+elif n>5:
+    print(n,"is greater than 5")
+else:
+    print(n,"is equal to 5")     
+    
+print("--- fourth code---")      
+number =[1,2,3,4,5,6,7] 
+sq=0
+for vol in number:
+    sq =vol *vol 
+    print("sq") 

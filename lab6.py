34f18a51587c3853d04dd85bc219d0ed32060b3d
komalpawar9973 <pawarkomal9973@gmail.com>
2026-08-24 11:16:06 +0530

    Delete lab6.py

--- lab6.py
+++
@@ -1,9 +0,0 @@
-expenses=0.0
-while True:
-    value= float(input("enter a value :")) 
-    
-    if value== -1:
-        break
-     
-    expenses =expenses+value
-    print("total expenses is:",expenses)
